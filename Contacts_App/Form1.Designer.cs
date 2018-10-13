d96c0c88948e4166beb26eb0b1de4a313e9363df
PRJadwin <30378471+PRJadwin@users.noreply.github.com>
2018-10-12 22:45:18 -0400

    first commit

--- Contacts_App/Form1.Designer.cs
+++ Contacts_App/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.delete = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.First_Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Last_Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(447, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(314, 289);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Contact, Full Info";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(302, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(453, 307);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(132, 50);
+            this.delete.TabIndex = 3;
+            this.delete.Text = "Delete Selected";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(623, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Add New Contact";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(453, 376);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Edit Selected";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.First_Name,
+            this.Last_Name});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(6, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(403, 388);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 40;
+            // 
+            // First_Name
+            // 
+            this.First_Name.Text = "First Name";
+            this.First_Name.Width = 190;
+            // 
+            // Last_Name
+            // 
+            this.Last_Name.Text = "Last Name";
+            this.Last_Name.Width = 195;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 413);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Contacts";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 436);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader First_Name;
+        private System.Windows.Forms.ColumnHeader Last_Name;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Contacts_App/Form1.cs
+++ Contacts_App/Form1.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Contacts_App
 {
     public partial class Form1 : Form
     {
+        public NpgsqlConnection conn;
+        public List<int> ids = new List<int>();
         public Form1()
         {
             InitializeComponent();
+            connect_DB();
+            fillList(conn);
+            conn.Close();
+        }
+        public void connect_DB()
+        {
+            string connstring = "Server=127.0.0.1; Port=5432; User ID=postgres; Password=irobot97; Database=Contacts;";
+            conn = new NpgsqlConnection(connstring);
+            conn.Open();
+        }
+        public void fillList(NpgsqlConnection conn)
+        {
+            string sql = "SELECT \"Contact_ID\", \"First Name\", \"Last Name\" FROM \"Contact\".\"Contacts\"";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+            NpgsqlDataReader rdr = cmd.ExecuteReader();
+            listView1.Items.Clear();
+            while (rdr.Read())
+            {
+                ListViewItem lv = new ListViewItem(rdr.GetInt32(0).ToString());
+                ids.Add(rdr.GetInt32(0));
+                lv.SubItems.Add(rdr.GetString(1).ToString());
+                lv.SubItems.Add(rdr.GetString(2).ToString());
+                listView1.Items.Add(lv);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string connstring = "Server=127.0.0.1; Port=5432; User ID=postgres; Password=irobot97; Database=Contacts;";
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                string sql = "SELECT \"Contact_ID\", \"First Name\", \"Last Name\", \"Date of Birth\", \"Address\", \"Phone #\", \"Email\" FROM \"Contact\".\"Contacts\" WHERE \"Contact_ID\" = " + listView1.SelectedIndices; 
+                
+                List<string> entries = new List<string>() { "First Name: ", "Last Name: ", "Date of Birth: ", "Address: ", "Phone #: ", "Email: " };
+
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+                listBox1.Items.Clear();
+                while (rdr.Read())
+                {
+                    Console.Write(rdr.GetString(1).ToString());
+                    Console.Write(rdr.GetString(2).ToString());
+                    Console.Write(rdr.GetString(3).ToString());
+                    Console.Write(rdr.GetString(4).ToString());
+                    Console.Write(rdr.GetString(5).ToString());
+                    Console.Write(rdr.GetString(6).ToString());
+                }
+                conn.Close();
+            }
+            catch (NpgsqlException)
+            {
+                MessageBox.Show(System.Environment.StackTrace.ToString(), "Err");
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show(System.Environment.StackTrace.ToString(), "Err");
+            }
         }
     }
 }
